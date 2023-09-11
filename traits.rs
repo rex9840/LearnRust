3a87b548c295f51819378ed723bb64572473882d
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-11 22:59:16 +0545

    polymerphism with traits

---
+++ traits.rs
@@ -0,0 +1,50 @@
+
+#![allow(dead_code)]
+
+struct SeaCreature
+{ 
+    pub name:String,
+    noise:String
+}
+
+impl SeaCreature
+{ 
+    pub fn get_sound(&self) -> &str
+    {
+            return &self.noise; 
+    } 
+}
+
+
+trait NoiseMaker  //interface and aggregrate 
+{ 
+    fn make_noise(&self);
+    
+    fn make_alot_of_noise(&self)
+    { 
+        self.make_noise();
+        self.make_noise();
+        self.make_noise();
+    } 
+}
+
+
+impl NoiseMaker for SeaCreature
+{
+    fn make_noise(&self)
+    {
+        println!("{0:?}",&self.get_sound());
+    }
+    
+}
+
+
+fn main()
+{ 
+    let  creature = SeaCreature{
+        name:String::from("ferris"),
+        noise:String::from("bulb")
+    };
+    creature.make_noise();
+    creature.make_alot_of_noise();
+}
