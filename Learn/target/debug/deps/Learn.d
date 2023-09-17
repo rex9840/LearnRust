7ca96de14d79d84cffcce2b9e08d69a0bf0fd87a
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-17 11:09:24 +0545

    library and crate creations

--- Learn/target/debug/deps/Learn.d
+++ Learn/target/debug/deps/Learn.d
@@ -1,7 +1,6 @@
-D:\Rust\Learn\target\debug\deps\Learn.exe: src\main.rs src\learn\mod.rs src\learn_rust.rs
+D:\Rust\Learn\target\debug\deps\Learn.exe: src\main.rs src\learn_rust.rs
 
-D:\Rust\Learn\target\debug\deps\Learn.d: src\main.rs src\learn\mod.rs src\learn_rust.rs
+D:\Rust\Learn\target\debug\deps\Learn.d: src\main.rs src\learn_rust.rs
 
 src\main.rs:
-src\learn\mod.rs:
 src\learn_rust.rs:
