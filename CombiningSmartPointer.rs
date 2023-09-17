f0ce021db1ef4691af3106129d899b543a994296
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-17 10:01:47 +0545

    using Rs and RefCell both working together
    using the collective ownership and borrow concept

---
+++ CombiningSmartPointer.rs
@@ -0,0 +1,56 @@
+use std::cell::RefCell;
+use std::rc::Rc; 
+
+struct Person
+{ 
+    tasks: u8
+}
+
+
+
+impl Person
+{ 
+    fn work(&mut self,name :&str)
+    { 
+        println!("{0:?} is working",name);
+        self.tasks -= 1;  
+    }
+}
+
+
+struct Worker
+{
+    name:String,
+    person:Rc<RefCell<Person>>
+}
+
+//Rc(refernce counter) collective ownership 
+//RefCell is a datastructure which gives us the mutable access to imuutable data 
+
+
+impl Worker
+{ 
+    fn task(&self)
+    {
+        let mut person = self.person.borrow_mut();
+        person.work(&self.name); 
+    }
+}
+
+fn main()
+{
+    let person = Rc::new(RefCell::new(Person{tasks:4}));
+    let hr= Worker {name:String::from("Ram"),person:person.clone()};
+
+    let manager= Worker {name:String::from("Shyam"),person:person.clone()} ;
+
+    manager.task(); 
+    hr.task(); 
+
+    let borrow_person = person.borrow();
+    println!("Remaining task : {0:?}",borrow_person.tasks); 
+
+
+
+}
+
